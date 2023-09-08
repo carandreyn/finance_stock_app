63612ff8bbb2e760ffa6675ff742ac792d5c62a6
Carlos Reynaga <Creynaga@allamericanasphalt.com>
2023-09-08 11:20:09 -0700

    added css and routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,16 @@
-import { useState, ChangeEvent, SyntheticEvent } from 'react';
+import { Outlet } from 'react-router';
 import './App.css';
-import CardList from './Components/CardList/CardList';
-import Search from './Components/Search/Search';
+import Navbar from './Components/Navbar/Navbar';
+
 
 
 function App() {
-  const [search, setSearch] = useState<string>();
-    
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-      setSearch(e.target.value);
-      console.log(e);
-  }
-
-  const onClick = (e: SyntheticEvent) => {
-      console.log(e);
-  }
   
   return (
-    <div className="App">
-      <Search onClick={onClick} search={search} handleChange={handleChange}/>
-      <CardList />
-    </div>
+    <>
+      <Navbar />
+      <Outlet />
+    </>
   );
 }
 
--- frontend/src/Components/Card/Card.tsx
+++ frontend/src/Components/Card/Card.tsx
@@ -1,26 +1,39 @@
-import React from 'react'
+import React, { SyntheticEvent } from "react";
 import "./Card.css";
+import { CompanySearch } from "../../company";
+import AddPortfolio from "../Portfolio/AddPortfolio/AddPortfolio";
+import { Link } from "react-router-dom";
 
 interface Props {
-    companyName: string;
-    ticker: string;
-    price: number;
+  id: string;
+  searchResult: CompanySearch;
+  onPortfolioCreate: (e: SyntheticEvent) => void;
 }
 
-const Card: React.FC<Props> = ({ companyName, ticker, price }: Props) : JSX.Element => {
+const Card: React.FC<Props> = ({
+  id,
+  searchResult,
+  onPortfolioCreate,
+}: Props): JSX.Element => {
   return (
-    <div className='card'>
-        <img
-        src="https://images.unsplash.com/photo-1612428978260-2b9c7df20150?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
-        alt="stock"
-        />
-        <div className='details'>
-            <h2>{companyName} ({ticker})</h2>
-            <p>${price}</p>
-        </div>
-        <p className='info'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, excepturi?</p>
+    <div
+      className="flex flex-col items-center justify-between w-full p-6 bg-slate-100 rounded-lg md:flex-row"
+      key={id}
+      id={id}
+    >
+      <Link to={`/company/${searchResult.symbol}`} className="font-bold text-center text-black md:text-left">
+        {searchResult.name} ({searchResult.symbol})
+      </Link>
+      <p className="text-black">{searchResult.currency}</p>
+      <p className="font-bold text-black">
+        {searchResult.exchangeShortName} - {searchResult.stockExchange}
+      </p>
+      <AddPortfolio
+        onPortfolioCreate={onPortfolioCreate}
+        symbol={searchResult.symbol}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
