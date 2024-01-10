4dc7e73e4fdb399c35efef42d38a4909cf6a1a89
Carlos Reynaga <carandreyn@gmail.com>
2024-01-09 16:22:16 -0800

    finished frontend

--- frontend/src/Pages/CompanyPage/CompanyPage.tsx
+++ frontend/src/Pages/CompanyPage/CompanyPage.tsx
@@ -1,15 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { CompanyProfile } from "../../company";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { getCompanyProfile } from "../../api";
 import Sidebar from "../../Components/Sidebar/Sidebar";
 import CompanyDashboard from "../../Components/CompanyDashboard/CompanyDashboard";
 import Tile from "../../Components/Tile/Tile";
+import CompFinder from "../../Components/CompFinder/CompFinder";
+import TenKFinder from "../../Components/TenKFinder/TenKFinder";
+import Spinner from "../../Components/Spinner/Spinner";
 
 interface Props {}
 
 const CompanyPage = (props: Props) => {
   let { ticker } = useParams();
+
   const [company, setCompany] = useState<CompanyProfile>();
 
   useEffect(() => {
@@ -25,12 +29,19 @@ const CompanyPage = (props: Props) => {
       {company ? (
         <div className="w-full relative flex ct-docs-disable-sidebar-content overflow-x-hidden">
           <Sidebar />
-          <CompanyDashboard>
+          <CompanyDashboard ticker={ticker!}>
             <Tile title="Company Name" subTitle={company.companyName} />
+            <Tile title="Price" subTitle={"$" + company.price.toString()} />
+            <Tile title="DCF" subTitle={"$" + company.dcf.toString()} />
+            <Tile title="Sector" subTitle={company.sector} />
+            <TenKFinder ticker={company.symbol} />
+            <p className="bg-white shadow rounded text-medium font-medium text-gray-900 p-3 mt-1 m-4">
+              {company.description}
+            </p>
           </CompanyDashboard>
         </div>
       ) : (
-        <div>Company Not Found!</div>
+        <Spinner />
       )}
     </>
   );
--- frontend/src/Routes/Routes.tsx
+++ frontend/src/Routes/Routes.tsx
@@ -1,10 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import HomePage from "../Pages/HomePage/HomePage";
-import SearchPage from "../Pages/SearchPage/SearchPage";
 import CompanyPage from "../Pages/CompanyPage/CompanyPage";
+import SearchPage from "../Pages/SearchPage/SearchPage";
 import CompanyProfile from "../Components/CompanyProfile/CompanyProfile";
 import IncomeStatement from "../Components/IncomeStatement/IncomeStatement";
+import DesignGuide from "../Pages/DesignPage/DesignPage";
+import BalanceSheet from "../Components/BalanceSheet/BalanceSheet";
+import CashflowStatement from "../Components/CashFlowStatement/CashFlowStatement";
+
 
 export const router = createBrowserRouter([
   {
@@ -13,12 +17,15 @@ export const router = createBrowserRouter([
     children: [
       { path: "", element: <HomePage /> },
       { path: "search", element: <SearchPage /> },
+      { path: "design-guide", element: <DesignGuide /> },
       {
-        path: "/company/:ticker",
+        path: "company/:ticker",
         element: <CompanyPage />,
         children: [
-          { path: "company-profile", element: < CompanyProfile/> },
+          { path: "company-profile", element: <CompanyProfile /> },
           { path: "income-statement", element: <IncomeStatement /> },
+          { path: "balance-sheet", element: <BalanceSheet /> },
+          { path: "cashflow-statement", element: <CashflowStatement /> },
         ],
       },
     ],
